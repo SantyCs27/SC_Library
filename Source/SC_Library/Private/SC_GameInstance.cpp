54e04b1113fa606f2f32343ab7b846f509814eea
SantyCs27 <81786408+SantyCs27@users.noreply.github.com>
2025-04-14 17:34:49 -0300

    Source

---
+++ Source/SC_Library/Private/SC_GameInstance.cpp
@@ -0,0 +1,33 @@
+﻿// Copyright Santiago Cañas. All Rights Reserved.
+
+
+#include "SC_GameInstance.h"
+#include "Runtime/UMG/Public/Blueprint/UserWidget.h"
+#include "MoviePlayer.h"
+
+void USC_GameInstance::Init()
+{
+	Super::Init();
+
+	FCoreUObjectDelegates::PreLoadMap.AddUObject(this, &USC_GameInstance::BeginLoadingScreen);
+	FCoreUObjectDelegates::PostLoadMapWithWorld.AddUObject(this, &USC_GameInstance::EndLoadingScreen);
+}
+
+void USC_GameInstance::BeginLoadingScreen_Implementation(const FString& InMapName)
+{
+	if (!IsRunningDedicatedServer())
+	{
+		FLoadingScreenAttributes LoadingScreen;
+		LoadingScreen.bAutoCompleteWhenLoadingCompletes = false;
+
+		UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(), LoadingScreenWidget);
+		LoadingScreen.WidgetLoadingScreen = Widget->TakeWidget();
+
+		GetMoviePlayer()->SetupLoadingScreen(LoadingScreen);
+	}
+}
+
+void USC_GameInstance::EndLoadingScreen_Implementation(UWorld* InLoadedWorld)
+{
+
+}
